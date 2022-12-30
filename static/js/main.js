c030eeef6c64da3f0304a7a7909508e90569bfd2
ListowelAdolwin <listoweladolwin@gmail.com>
2022-12-30 22:40:22 +0000

    Updated ajax to the upvote, downvote and save buttons

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $('.like-btn').click(function (e) {
+    e.preventDefault();
+
+    var url = $(this).attr('href');
+    myFunction();
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      success: function (data) {},
+    });
+  });
+});
+
+function myFunction() {
+  alert("Hello from a static file!");
+}
